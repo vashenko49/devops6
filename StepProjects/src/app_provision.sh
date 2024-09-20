16294d55df22a9e89061f5d5421bd75b8a1d4870
Serhii Vashchenko <vashenko49@gmail.com>
2024-09-20 14:00:25 +0300

    upload project

--- StepProjects/src/app_provision.sh
+++ StepProjects/src/app_provision.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-# Update and install dependencies
 sudo apt-get update
 sudo apt-get install -y openjdk-11-jdk git maven
 
-# Add a non-root user to run the application
 sudo useradd -m -s /bin/bash appuser
 
-# Clone the PetClinic repository
 sudo -u appuser git clone https://github.com/vashenko49/PetClinic.git /home/appuser/spring-petclinic
 
-
-# Navigate to the project directory
 cd /home/appuser/spring-petclinic
 
 sudo -u appuser mvn package
 
-export MYSQL_URL=jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME
-export MYSQL_USER=$DB_USER
-export MYSQL_PASS=$DB_PASS
+sudo chmod 644  /home/appuser/spring-petclinic/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar
 
-sudo -u appuser java -jar /home/appuser/spring-petclinic/target/*.jar --spring.profiles.active=mysql
+sudo tee /etc/systemd/system/petclinic.service > /dev/null <<EOF
+[Unit]
+Description=My Spring Boot Application
+After=network.target
+
+[Service]
+User=appuser
+ExecStart=/usr/bin/java -jar /home/appuser/spring-petclinic/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar --spring.profiles.active=mysql
+Restart=always
+WorkingDirectory=/home/appuser/spring-petclinic
+
+Environment=PROFILE=mysql
+Environment=MYSQL_URL=jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_NAME}
+Environment=MYSQL_PASS=${DB_PASS}
+Environment=MYSQL_USER=${DB_USER}
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable petclinic.service
+sudo systemctl start petclinic.service
